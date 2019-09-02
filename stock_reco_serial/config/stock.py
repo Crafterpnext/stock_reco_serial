67f046fd73540873070e765f2cf4c7ac8ad3a76e
craft <riyaspangode@gmail.com>
2019-09-02 17:15:48 +0400

    stock reco menu

---
+++ stock_reco_serial/config/stock.py
@@ -0,0 +1,17 @@
+from __future__ import unicode_literals
+from frappe import _
+
+def get_data():
+        return [
+                {
+                        "label": _("Tools"),
+                        "icon": "fa fa-wrench",
+                        "items": [
+                                {
+                                        "type": "doctype",
+                                        "name": "Serialized Stock Reconciliation",
+                                        "onboard": 1,
+                                },
+                        ]
+                }               
+        ]     
